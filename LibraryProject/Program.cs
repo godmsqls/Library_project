b720f2b72af96ff9ac6e96553c3a7f5f1db7663a
louisnhak-crypto <louisnhak@gmail.com>
2026-05-14 18:29:43 +0900

    Update Program.cs

--- LibraryProject/Program.cs
+++ LibraryProject/Program.cs
@@ -2,7 +2,6 @@
 using System.Windows.Forms;
 using LibraryProject.Views.Auth;
 using LibraryProject.Controllers;
-using LibraryProject.Models;
 
 namespace LibraryProject
 {
@@ -11,11 +10,8 @@ namespace LibraryProject
         [STAThread]
         static void Main(string[] args)
         {
-            var user = new User("testuser9", "1234", "테스트", "Member");
-            user.InsertUser();
-            MessageBox.Show("삽입 성공");
-            //AuthController authController = new AuthController();
-            //authController.ShowAuthView();
+            AuthController authController = new AuthController();
+            authController.ShowAuthView();
         }
     }
 }
