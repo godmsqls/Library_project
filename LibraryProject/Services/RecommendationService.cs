4129455ee4781570e9ef58fe2aa46b4f791d7646
peterbae9327 <peterbae9327@kw.ac.kr>
2026-05-14 12:07:17 +0900

    Fix: Defined User-Agent

--- LibraryProject/Services/RecommendationService.cs
+++ LibraryProject/Services/RecommendationService.cs
@@ -95,6 +95,7 @@ namespace LibraryProject.Services
         private readonly HttpClient _httpClient = new HttpClient();
         public async Task<Dictionary<int, CategoryNode>> InitializeAsync()
         {
+            _httpClient.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36");
             string html = await _httpClient.GetStringAsync(BLOG_URL);
             string sheetId = GetSheetId(html);
             if (sheetId != null && IsUpdateNeeded(sheetId).Result)
